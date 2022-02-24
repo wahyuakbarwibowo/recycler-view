a459c1c788769e19be4d4c5b619c1417c404248f
Wahyu Akbar Wibowo <wahyuwibowoworker@gmail.com>
2022-02-24 15:34:34 +0700

    adjusment data class Hero

--- app/src/main/java/me/wahyu/recyclerview/Hero.kt
+++ app/src/main/java/me/wahyu/recyclerview/Hero.kt
@@ -1,7 +1,7 @@
 package me.wahyu.recyclerview
 
-class Hero {
-    var name: String = ""
-    var detail: String = ""
+data class Hero (
+    var name: String = "",
+    var detail: String = "",
     var photo: Int = 0
-}
+)
